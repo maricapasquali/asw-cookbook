550d5a67ac38be12e4087cfc700feff128395930
Marica Pasquali <marica.pasquali@studio.unibo.it>
2022-01-03 21:30:07 +0100

    session api (separation from user api)

--- cookbook-server/modules/utilities/index.ts
+++ cookbook-server/modules/utilities/index.ts
@@ -1,28 +1,4 @@
-import {IUser} from "../../models/schemas/user";
-
 import * as crypto from 'crypto'
-
-type AuthorizationValue = {userID: string, password: string} | {access_token: string}
-
-export function extractAuthorization(headers): AuthorizationValue | any {
-    if(!headers.authorization) return {access_token: undefined}
-    const [type, value] = headers.authorization.split(' ')
-    switch (type){
-        case 'Basic': {
-            let buff = Buffer.from(value, 'base64');
-            let [userID, password] = buff.toString('utf-8').split(':');
-            return {userID: userID, password: password}
-        }
-        case 'Bearer': {
-            return {access_token: value}
-        }
-        default: throw new Error(type + ' not implemented')
-    }
-}
-
-export function isAlreadyLoggedOut(user: IUser): boolean {
-    return !user.credential.tokens || typeof user.credential.tokens === 'number'
-}
 
 export function unixTimestampToString(ts: number, get: string = 'date_time', locals: string = 'it-IT', options: object = {}): string {
     let date = new Date(ts)
