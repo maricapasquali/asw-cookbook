8578437d9804536d40325da1938531f22d6a7f44
Marica Pasquali <marica.pasquali@studio.unibo.it>
2022-03-23 18:41:06 +0100

    create view and api for signup admin (available only in server side)

--- cookbook-server/routes/index.ts
+++ cookbook-server/routes/index.ts
@@ -1,24 +1,22 @@
 import userRoute from './user'
+import adminRoute from './user/admin'
 import friendRoute from './user/friend'
 import foodRoute from './food'
 import shoppingListRoute from './shopping-list'
 import recipeRoute from './recipe'
 import notificationRoute from './notification'
 import chatRoute from './chat'
-import * as path from "path";
+import filesRoute from './files'
 
 export default function (app: any) {
-    app.get('/images/:filename', (req, res) => res.status(200).sendFile(
-        path.resolve('cookbook-server/images/' + req.params.filename)
-    ))
-    app.get('/videos/:filename', (req, res) => res.status(200).sendFile(
-        path.resolve('cookbook-server/videos/' + req.params.filename)
-    ))
     userRoute(app)
+    adminRoute(app)
     friendRoute(app)
     foodRoute(app)
     shoppingListRoute(app)
     recipeRoute(app)
     notificationRoute(app)
     chatRoute(app)
+
+    filesRoute(app)
 }
