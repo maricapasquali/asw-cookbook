1c91947f508f2c7aab83884e8faadff347057c10
Marica Pasquali <marica.pasquali@studio.unibo.it>
2022-04-25 20:07:42 +0200

    fix fetch data without reload page

--- cookbook-app/src/components/app/handler-errors/unauthenticated-error-handler.vue
+++ cookbook-app/src/components/app/handler-errors/unauthenticated-error-handler.vue
@@ -1,11 +1,11 @@
 <template>
   <b-modal v-model="value.show" title="Sessione scaduta" centered
-           @ok="continueHere" @cancel="goLogin" @hide="close" hide-header-close no-close-on-esc no-close-on-backdrop>
+           @ok="continueHere" @cancel="goLogin" hide-header-close no-close-on-esc no-close-on-backdrop>
     <template #modal-ok>Continua</template>
     <template #modal-cancel>Login</template>
     <template #default>
       <p>La sessione di accesso è scaduta.</p>
-      <p>Puoi rieffettuato l'accesso o continuare senza effettuare l'accesso.</p>
+      <p>Puoi rieffettuato l'accesso o continuare come utente ospite.</p>
     </template>
   </b-modal>
 </template>
@@ -19,11 +19,6 @@ export default {
       message: String
     }
   },
-  data(){
-    return {
-      _forceRenderRoute: false
-    }
-  },
   methods: {
     _reset(){
       this.$store.dispatch('reset')
@@ -35,22 +30,7 @@ export default {
     },
     continueHere(){
       this._reset()
-      this.$data._forceRenderRoute = this.$route.name === 'homepage' || !this.value._forbiddenPage
-      if(this.$data._forceRenderRoute) {
-        console.debug('Force update router ...')
-        this.$emit('force-render-router')
-      }
       this.$router.replace({ name: this.value._forbiddenPage ? 'homepage' : this.$route.name })
-    },
-    _resetForceRenderRoute(){
-      console.debug('Reset force update router')
-      this.$emit('close')
-    },
-    close(){
-      if(this.$data._forceRenderRoute) {
-        this.$data._forceRenderRoute = false
-        setTimeout(this._resetForceRenderRoute.bind(this), 1)
-      }
     }
   }
 }
