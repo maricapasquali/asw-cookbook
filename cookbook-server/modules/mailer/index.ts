ebb2f017ee9b269d6ade4a123fd13b76c275f5a8
Marica Pasquali <marica.pasquali@studio.unibo.it>
2022-05-12 22:24:19 +0200

    change email template creation

--- cookbook-server/modules/mailer/index.ts
+++ cookbook-server/modules/mailer/index.ts
@@ -3,29 +3,33 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 import * as dotenv from 'dotenv'
-dotenv.config({path: path.resolve(__dirname, ".env")})
-
-export interface IMailer {
-   send(messageHeader: MessageHeader, callbacks?: {error: (e: any) => void, success: (i: object) => void})
-   save(filename: string, content, callback?: (err) => void)
-}
+import {TemplateEmail} from "./templates";
+dotenv.config({path: path.join(__dirname, ".env")})
 
 export interface MessageHeader {
    from?: string
    to: string
    subject: string
-   html: string
-   text?: string
    attachments?: []
 }
 
-export class Mailer implements IMailer{
+export type MailerOptions = {
+   test ?: boolean,
+   savedJSON ?: { filename: string },
+   callbacks ?: { error: (e: any) => void, success: (i: object) => void }
+}
+
+export interface IMailer {
+   send(messageHeader: MessageHeader, templateEmail: TemplateEmail, options?: MailerOptions)
+}
+
+export class Mailer implements IMailer {
    private readonly transport: any
    private readonly from: string
 
    private readonly output;
 
-   constructor(from: string, output?: string ) {
+   constructor(from: string, output?: string) {
       this.from = from
       this.transport = nodemailer.createTransport({
          host: process.env.MAILER_HOST,
@@ -38,30 +42,42 @@ export class Mailer implements IMailer{
       this.output = output || path.join(__dirname , 'outputs')
    }
 
-   send(messageHeader: MessageHeader, callbacks?: {error: (e: any) => void, success: (i: object) => void}){
-     try {
-        const message = Object.assign({from: this.from}, messageHeader)
-        this.transport.sendMail(message, function (err, info){
-           if(err) {
-              console.error(err)
-              if(callbacks && typeof callbacks.error === 'function') callbacks.error(err)
-           }
-           else {
-              console.debug('Email was sent to '+ messageHeader.to)
-              if(callbacks && typeof callbacks.success === 'function') callbacks.success(info)
-           }
-        })
-     }catch (e){
-        console.error(e)
-        if(callbacks && typeof callbacks.error === 'function') callbacks.error(e)
-     }
+   send(messageHeader: MessageHeader, templateEmail: TemplateEmail, options?: MailerOptions) {
+      const callbacks = options.callbacks
+      const savedJSON = options.savedJSON
+      const test = options.test
+
+      try {
+         const _messageHeader = Object.assign({ from: this.from }, messageHeader)
+         const message = {..._messageHeader, html: templateEmail.toHtml(), text: templateEmail.toText() }
+
+         if(!test){
+            this.transport.sendMail(message, function (err, info){
+               if(err) {
+                  console.error(err)
+                  if(callbacks && typeof callbacks.error === 'function') callbacks.error(err)
+               }
+               else {
+                  console.debug('Email was sent to '+ messageHeader.to)
+                  if(callbacks && typeof callbacks.success === 'function') callbacks.success(info)
+               }
+            })
+         }
+
+         if(savedJSON){
+            let templateEmailJSON = templateEmail.toJson()
+            let pathFileJSON: string = path.join(this.output, savedJSON.filename + ".json")
+            fs.writeFile(pathFileJSON, JSON.stringify({..._messageHeader, ...templateEmailJSON}), 'utf8', (err)=> {
+               if(err) console.error(err)
+               else console.debug('Save email in '+ pathFileJSON)
+            });
+         }
+
+      }catch (e){
+         console.error(e)
+         if(callbacks && typeof callbacks.error === 'function') callbacks.error(e)
+      }
    }
 
-   save(filename: string, content, callback?: (err) => void) {
-      fs.writeFile(path.join(this.output , filename), content, callback? callback: (e)=> {
-         if(e) console.error(e.message)
-         else console.debug('Save email in '+ this.output)
-      });
-   }
 }
 
